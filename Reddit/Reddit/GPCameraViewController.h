a395d3b87231ad4e103afd908d49b40f814dcd25
Luis Mendoza <luis@mendoza.io>
2014-08-05 22:18:59 -0400

    Added Second Storyboard and fixed flow.

---
+++ Reddit/Reddit/GPCameraViewController.h
@@ -0,0 +1,13 @@
+//
+//  GPCameraViewController.h
+//  Reddit
+//
+//  Created by Luis Mendoza on 8/5/14.
+//  Copyright (c) 2014 GlobalPark. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GPCameraViewController : UITableViewController
+
+@end
