b730c51194aaf88363515be9861c7cfad00de2b1
Luis Mendoza <luis@mendoza.io>
2014-08-20 18:34:51 -0400

    Revert b65a9d2..c546e40
    This rolls back to commit b65a9d21119076881e1771f90cee78bec33463fe.

--- todo/db/schema.rb
+++ todo/db/schema.rb
@@ -11,16 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140820220626) do
-
-  create_table "todo_items", force: true do |t|
-    t.integer  "todo_list_id"
-    t.string   "content"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "todo_items", ["todo_list_id"], name: "index_todo_items_on_todo_list_id"
+ActiveRecord::Schema.define(version: 20140819234929) do
 
   create_table "todo_lists", force: true do |t|
     t.string   "title"
