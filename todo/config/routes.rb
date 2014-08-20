26ebcb2751b0331dd7225963192e84511bce5ea7
Luis Mendoza <luis@mendoza.io>
2014-08-20 18:40:10 -0400

    Revert b65a9d2..4945357
    This rolls back to commit b65a9d21119076881e1771f90cee78bec33463fe.

--- todo/config/routes.rb
+++ todo/config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  resources :todo_lists do
-    resources :todo_items
-  end
+  resources :todo_lists
   root 'todo_lists#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
