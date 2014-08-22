e2cdc1409844161bd338d6f6c918b8d50fc0ef48
Luis Mendoza <luis@mendoza.io>
2014-08-22 18:43:58 -0400

    Added delete funcionality

--- todo/app/controllers/todo_items_controller.rb
+++ todo/app/controllers/todo_items_controller.rb
@@ -1,15 +1,16 @@
 class TodoItemsController < ApplicationController
+
+  before_action :find_todo_list
+
   def index
-  	@todo_list = TodoList.find(params[:todo_list_id])
+  	
   end
 
   def new
-  	@todo_list = TodoList.find(params[:todo_list_id])
   	@todo_item = @todo_list.todo_items.new
   end
 
   def create
-  	@todo_list = TodoList.find(params[:todo_list_id])
   	@todo_item = @todo_list.todo_items.new(todo_item_params)
 
   	if @todo_item.save
@@ -22,12 +23,20 @@ class TodoItemsController < ApplicationController
   end
 
   def edit
-    @todo_list = TodoList.find(params[:todo_list_id])
     @todo_item = @todo_list.todo_items.find(params[:id])
   end
 
+  def destroy
+    @todo_item = @todo_list.todo_items.find(params[:id])
+    if @todo_item.destroy
+      flash[:success] = "Todo item was deleted"
+    else
+      flash[:error] = "Todo Item could not be deleted"
+    end
+    redirect_to todo_list_todo_items_path
+  end
+
   def update
-    @todo_list = TodoList.find(params[:todo_list_id])
     @todo_item = @todo_list.todo_items.find(params[:id])
 
     if @todo_item.update_attributes(todo_item_params)
@@ -47,5 +56,9 @@ class TodoItemsController < ApplicationController
   	def todo_item_params
   		params[:todo_item].permit(:content)
   	end
+
+  def find_todo_list
+    @todo_list = TodoList.find(params[:todo_list_id])
+  end
 
 end
