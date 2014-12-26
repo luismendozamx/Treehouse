d684161bdbf7e85447206c1286e08da10689269a
Luis Mendoza <luis@mendoza.io>
2014-12-26 02:07:38 -0600

    Added password reset functionality

--- Ruby/odot/db/schema.rb
+++ Ruby/odot/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141222050054) do
+ActiveRecord::Schema.define(version: 20141226060235) do
 
   create_table "todo_items", force: true do |t|
     t.integer  "todo_list_id"
@@ -40,8 +40,10 @@ ActiveRecord::Schema.define(version: 20141222050054) do
     t.string   "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "password_reset_token"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", using: :btree
+  add_index "users", ["password_reset_token"], name: "index_users_on_password_reset_token", using: :btree
 
 end
