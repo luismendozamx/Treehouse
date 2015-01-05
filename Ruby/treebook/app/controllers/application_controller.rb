e0a8fb7473ebaa3db4d83191e7d1fc223b5dee4e
Luis Mendoza <luis@mendoza.io>
2015-01-05 16:34:36 -0600

    Changed sign up parameters allowed.

--- Ruby/treebook/app/controllers/application_controller.rb
+++ Ruby/treebook/app/controllers/application_controller.rb
@@ -8,6 +8,6 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:user_name, :email, :first_name, :last_name, :password, :password_confirm) }
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:user_name, :email, :first_name, :last_name, :password, :password_confirm) }
   end
 end
