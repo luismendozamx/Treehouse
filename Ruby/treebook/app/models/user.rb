d2ba928c88eb20c3b84a26548cf6b441f86325a4
Luis Mendoza <luis@mendoza.io>
2015-01-05 19:54:04 -0600

    Added user field validations

--- Ruby/treebook/app/models/user.rb
+++ Ruby/treebook/app/models/user.rb
@@ -6,6 +6,14 @@ class User < ActiveRecord::Base
 
   has_many :statuses
 
+  validates :first_name, :last_name, :user_name, :email, presence: true
+  validates :user_name, :email, uniqueness: true
+
+  validates :user_name, format: {
+                           with: /\a-zA-Z0-9_-/,
+                           message: "must be formatted correctly."
+                         }
+
   def full_name
     first_name + " " + last_name
   end
