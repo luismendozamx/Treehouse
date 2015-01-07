be0e4520267ff5f24e9816f0523cdc77d7669b30
Luis Mendoza <luis@mendoza.io>
2015-01-06 20:38:32 -0600

    Added alerts, new routes and scoped posting

--- Ruby/treebook/config/routes.rb
+++ Ruby/treebook/config/routes.rb
@@ -1,7 +1,19 @@
 Rails.application.routes.draw do
+  
+  # Devise Routes
   devise_for :users
-  resources :statuses
 
+  devise_scope :user do
+    get 'register', to: 'devise/registrations#new', as: :register
+    get 'login', to: 'devise/sessions#new', as: :login
+    get 'logout', to: 'devise/sessions#destroy', as: :logout
+  end
+
+  # Status Routes
+  resources :statuses
+  get 'feed', to: 'statuses#index', as: :feed
+
+  # Root
   root to: 'statuses#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
